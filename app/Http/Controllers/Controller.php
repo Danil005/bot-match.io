32b11b234ae7fd5025cbfbca9625bd338e9948f5
Fiks <danilsidorenko00@gmail.com>
2021-11-07 15:13:47 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    protected function declOfNum($number, $titles): string
+    public function declOfNum($number, $titles): string
     {
         $cases = [2, 0, 1, 1, 1, 2];
         $format = $titles[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]];
